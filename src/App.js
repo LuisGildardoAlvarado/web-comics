3d91df19126458e31e950ea864e571271a3d98d6
LuisGildardoAlvarado <luisgil9900@gmail.com>
2022-06-29 20:59:43 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { FiAlignJustify, FiShoppingBag, FiSearch, FiUser } from "react-icons/fi";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='header-nav'>
+          <div className='container-menu'>
+            <a href='null'> <FiAlignJustify/> </a>
+          </div>
+
+          <div className='container-logo'>
+                <a href='null'> <h1>Web Comics</h1> </a>
+              </div>
+
+          <div className='container-icons'>
+            <ul>
+              <li><a href='null'> <FiSearch/> </a></li>
+              <li><a href='null'> <FiShoppingBag/> </a></li>
+              <li><a href='null'> <FiUser/> </a></li>
+            </ul>
+          </div>
+        </div>
       </header>
     </div>
   );
