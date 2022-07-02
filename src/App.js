2ee1abd7e82e8942198de9401537635b64378dd6
LuisGildardoAlvarado <luisgil9900@gmail.com>
2022-07-01 19:37:43 -0700

    barra de busqueda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
-import { FiAlignJustify, FiShoppingBag, FiSearch, FiUser } from "react-icons/fi";
+import { FiSearch, FiUser } from "react-icons/fi";
 
 function App() {
   return (
     <div className="App">
       <header>
         <div className='header-nav'>
-          <div className='container-menu'>
-            <a href='null'> <FiAlignJustify/> </a>
+          <div className='container-logo'>
+            <a href='null'> <h1>Web Comics</h1> </a>
           </div>
 
-          <div className='container-logo'>
-                <a href='null'> <h1>Web Comics</h1> </a>
-              </div>
+          <div className='container-search'>
+            <input type="text" className='search-input' placeholder='Search...' />
+            <button className='search-button'>
+              <a href='null'> <FiSearch/> </a>
+            </button>
+            
+          </div>
 
           <div className='container-icons'>
             <ul>
-              <li><a href='null'> <FiSearch/> </a></li>
-              <li><a href='null'> <FiShoppingBag/> </a></li>
               <li><a href='null'> <FiUser/> </a></li>
             </ul>
           </div>
